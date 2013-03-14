ffe4b0bbc17a53455445a814699cd07f7c6676ca
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-14 15:31:48 +1100

    Being a good subclass.

--- gs/profile/image/square/contentprovider.py
+++ gs/profile/image/square/contentprovider.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
+from base64 import b64encode
 from zope.cachedescriptors.property import Lazy
-from zope.contentprovider.interfaces import UpdateNotCalled
-from zope.pagetemplate.pagetemplatefile import PageTemplateFile
 from userimage import SquareUserImage
 from gs.profile.image.base.contentprovider import UserImageContentProvider
 
@@ -12,29 +11,20 @@ class SquareUserImageContentProvider(UserImageContentProvider):
     def __init__(self, context, request, view):
         super(SquareUserImageContentProvider, self).__init__(context, request,
                                                                 view)
-        self.__updated = False
-
-    def update(self):
-        self.__updated = True
-        self.pageTemplate = PageTemplateFile(self.pageTemplateFileName)
-
-    def render(self):
-        if not self.__updated:
-            raise UpdateNotCalled
-        return self.pageTemplate(view=self)
 
     @Lazy
     def userImage(self):
         retval = SquareUserImage(self.context, self.userInfo)
         return retval
 
-    @Lazy
-    def userImageUrl(self):
-        retval = self.missingImage
-        try:
-            if (not(self.userInfo.anonymous) and self.userImage.imagePath):
-                r = '{profile}/gs-profile-image-square/{size}'
-                retval = r.format(profile=self.userInfo.url, size=self.size)
-        except IOError:
-            pass  # Use the missingImage
+    def profile_image_link(self):
+        r = '{profile}/gs-profile-image-square/{size}'
+        retval = r.format(profile=self.userInfo.url, size=self.size)
+        return retval
+
+    def embedded_profile_image(self):
+        smallImage = self.userImage.get_resized(self.size)
+        d = b64encode(smallImage.data)
+        r = 'data:{mediatype};base64,{data}'
+        retval = r.format(mediatype=smallImage.contentType, data=d)
         return retval
