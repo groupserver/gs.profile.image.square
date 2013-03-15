4f24fb6e0efb070f53a9d0ff87d5fce37a4d0550
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-16 00:20:22 +1100

    Following the changes to the base class

--- gs/profile/image/square/contentprovider.py
+++ gs/profile/image/square/contentprovider.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from base64 import b64encode
 from zope.cachedescriptors.property import Lazy
 from zope.pagetemplate.pagetemplatefile import PageTemplateFile
 from userimage import SquareUserImage
@@ -8,10 +7,22 @@ from gs.profile.image.base.contentprovider import UserImageContentProvider
 
 class SquareUserImageContentProvider(UserImageContentProvider):
     """The square user image."""
+    def __init__(self, context, request, view):
+        super(SquareUserImageContentProvider, self).__init__(context, request,
+                                                                view)
 
     def update(self):
-        self.updated = True
+        super(SquareUserImageContentProvider, self).update()
         self.pageTemplate = PageTemplateFile(self.pageTemplateFileName)
+
+    def get_final_size(self):
+        if self.showMissingImage:
+            retval = [int(d) for d in (self.size, self.size)]
+        elif self.resizeNeeded:
+            retval = self.smallImage.getImageSize()
+        else:
+            retval = self.userImage.getImageSize()
+        return retval
 
     @Lazy
     def userImage(self):
@@ -19,27 +30,24 @@ class SquareUserImageContentProvider(UserImageContentProvider):
         return retval
 
     @Lazy
-    def userImageUrl(self):
-        retval = self.missingImage  # From the interface
-        try:
-            if (not(self.userInfo.anonymous)
-                and (self.userImage.file is not None)):
-                if int(self.size) >= 40:
-                    retval = self.profile_image_link()
-                else:
-                    retval = self.embedded_profile_image()
-        except IOError:
-            pass  # Use the missingImage
+    def resizeNeeded(self):
+        retval = (int(self.size) != self.userImage.width
+                    != self.userImage.height)
+        return retval
+
+    @Lazy
+    def smallImage(self):
+        if self.resizeNeeded:
+            retval = self.userImage.get_resized(int(self.size))
+        else:
+            retval = self.userImage
         return retval
 
     def profile_image_link(self):
-        r = '{profile}/gs-profile-image-square/{size}'
-        retval = r.format(profile=self.userInfo.url, size=self.size)
+        r = '{profile}/gs-profile-image-square'
+        retval = r.format(profile=self.userInfo.url)
         return retval
 
-    def embedded_profile_image(self):
-        smallImage = self.userImage.get_resized(self.size)
-        d = b64encode(smallImage.data)
-        r = 'data:{mediatype};base64,{data}'
-        retval = r.format(mediatype=smallImage.contentType, data=d)
-        return retval
+    def resize_link(self):
+        r = '{profileLink}/{size}'
+        return r.format(profileLink=self.profile_image_link(), size=self.size)
